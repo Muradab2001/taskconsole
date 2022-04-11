7524bfb9f572fe369b49afd28585cec1d8d68583
unknown <=>
2022-04-12 02:19:04 +0400

    end

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace usernametask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asagidaki member-lerden ibaret User class-i yaradin:
+            //UserName - istifadeci adini ifade edir
+            //Password - istifadeci sifresini ifade edir
+            //UserName deyeri teyin olunmamis User obyekti yaradila bilinmemelidir.UserName deyerinin uzunlugu minimum 8 olmalidr.
+            //Password deyerinin uzunlugu minimum 8 olmalidir ve icinde
+            //en az 1 boyuk herf,1 kicik herf ve 1 reqem olmalidir.Eks
+            //teqdirde password deyeri set oluna bilinmemelidir.
+            //Layihe run olduqda console penceresinden isitfadeciden username
+            //daxil etmesi istenilmelidir.Eger daxil edilen username UserName memberinin
+            //sertlerini odemirse istifadeciden yeniden username daxil etmesi istenilmelidir
+            //(duz yazanadek bu proses davam etmelidir). Daha sonra console penceresinden password deyerini daxil etmesi istenilmelidir.
+            //Eger daxil edilen deyer Password memberinin sertlerini odemirse yeniden daxil edilmesi istenilmelidir ta ki duz yazana qeder.
+            //Her iki deyer duz olduqdan sonra User obyekti yaradilir. (Encapsulatin tetqibi mutleqdir !)
+            Console.WriteLine("istifadeci adin daxil edin");
+            string username = Console.ReadLine();
+            Console.WriteLine("password daxil edin");
+            string password = Console.ReadLine();
+            userdata userpaschech = new userdata();
+            userpaschech.User = username;
+            userpaschech.Password = password;
+            Console.WriteLine(userpaschech.User) ;
+            
+        }
+    }
+}
---
+++ userdata.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace usernametask
+{
+    class userdata
+    {
+        private string _user;
+        private string _password;
+
+        public string Password {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                int count = 0;
+                int count1 = 0;
+                int count2 = 0;
+                if (value.Length >=8)
+                {
+                    foreach (char item in value)
+                    {
+                        if (char.IsUpper(item))
+                        {
+                            count++;
+                        }
+                        else if(char.IsDigit(item))
+                        {
+                            count1++;
+                        }
+                       else if (char.IsLetter(item))
+                        {
+                            count2++;
+                        }
+                    }
+                  
+                }
+                else
+                {
+                    Console.WriteLine("parol 8 den cox olmalidir");
+                }
+                if (count > 0 && count1 > 0 && count2 > 0)
+                {
+                    _password = Password;
+                }
+                else if(count==0)
+                {
+                    Console.WriteLine("boyuk herif yoxdur");
+                }
+                else if (count1 == 0)
+                {
+                    Console.WriteLine("reqem yoxdur");
+                }
+                else if (count2 == 0)
+                {
+                    Console.WriteLine("balaca herif yoxdur");
+                }
+            }
+        }
+        public string User
+        {
+            get
+            {
+                return _user;
+            }
+            set
+            {
+               
+                if (value.Length > 8)
+                {
+                    _user = User;
+
+                }
+                else
+                {
+                    Console.WriteLine("istifadeci adi 8 den cox olmalidir");
+                }
+
+            }
+        }
+    }
+    
+}
